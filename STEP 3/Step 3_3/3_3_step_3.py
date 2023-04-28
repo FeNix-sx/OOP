85af890374ea88e6611c58e12cd794cb91e5d272
FeNix <fenix_sx@mail.ru>
2023-04-28 23:06:26 +0300

    24042023 шаг 3.3_ 5 1 версия

--- STEP 3/Step 3_3/3_3_step_3.py
+++ STEP 3/Step 3_3/3_3_step_3.py
@@ -1,39 +1,47 @@
 """
-Подвиг 2. Объявите класс RandomPassword для генерации случайных паролей. Объекты этого класса должны создаваться командой:
+Подвиг 2. Объявите класс с именем Book (книга), объекты которого создаются командой:
 
-rnd = RandomPassword(psw_chars, min_length, max_length)
-где psw_chars - строка из разрешенных в пароле символов; min_length, max_length - минимальная и максимальная длина генерируемых паролей.
+book = Book(title, author, pages)
+где title - название книги (строка); author - автор книги (строка); pages - число страниц в книге (целое число).
 
-Непосредственная генерация одного пароля должна выполняться командой:
+Также при выводе информации об объекте на экран командой:
 
-psw = rnd()
-где psw - ссылка на строку длиной в диапазоне [min_length; max_length] из случайно выбранных символов строки psw_chars.
+print(book)
+должна отображаться строчка в формате:
 
-С помощью генератора списка (list comprehension) создайте список lst_pass из трех сгенерированных паролей объектом rnd класса RandomPassword, созданного с параметрами:
+"Книга: {title}; {author}; {pages}"
 
-min_length = 5
-max_length = 20
-psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
-P.S. Выводить на экран ничего не нужно, только создать список из паролей.
+Например:
 
-P.P.S. Дополнительное домашнее задание: попробуйте реализовать этот же функционал с использованием замыканий функций.
+"Книга: Муму; Тургенев; 123"
+
+Прочитайте из входного потока строки с информацией по книге командой:
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+(строки идут в порядке: title, author, pages). Создайте объект класса Book и выведите его строковое представление в консоль.
+
+Sample Input:
+
+Python ООП
+Балакирев С.М.
+1024
+Sample Output:
+
+Книга: Python ООП; Балакирев С.М.; 1024
 """
-from random import randint, sample
-class RandomPassword:
-    def __init__(self,psw_chars, min_length, max_length):
-        self.psw_chars= psw_chars
-        self.min_length = min_length
-        self.max_length = max_length
-    # Долго не мог понять, что надо еще что-то кроме класса создавать)
-    def __call__(self, *args, **kwargs):
-        return "".join(sample(self.psw_chars, randint(self.min_length, self.max_length+1)))
+import sys
+
+class Book:
+    def __init__(self, title, author, pages):
+        self.__title = title
+        self.__author = author
+        self.__pages = pages
+
+    def  __str__(self):
+        return f"Книга: {self.__title}; {self.__author}; {self.__pages}"
 
 
-min_length = 5
-max_length = 20
-psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
-rnd = RandomPassword(psw_chars, min_length, max_length)
-psw = rnd()
-lst_pass = [psw for _ in range(3)]
-
-print(lst_pass)
+lst_in = list(map(str.strip, sys.stdin.readlines())) # считывание списка из входного потока (эту строчку не менять)
+title, author, pages = lst_in
+book = Book(title, author, pages)
+print(book)
