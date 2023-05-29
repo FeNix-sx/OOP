aa26c558126c9ba043a26025df0056915d585a1a
FeNix <fenix_sx@mail.ru>
2023-05-29 20:53:49 +0300

    29052023 шаг 3.4.9 завершен

--- STEP 3/Step 3_4/3_4_step_9.py
+++ STEP 3/Step 3_4/3_4_step_9.py
@@ -40,3 +40,61 @@ for x in my_budget.get_items():
     s = s + x
 P.S. В программе требуется только объявить класс. На экран ничего выводить не нужно.
 """
+from typing import Optional, Union, Type
+
+class Item:
+    def __init__(self, name: str=None, money: float=None) -> None:
+        self.name = name
+        self.money = money
+
+    @staticmethod
+    def __check_item(item: Union[int,float, Type['Item']]) -> float|int:
+        if type(item) in (int, float):
+            return item
+        elif isinstance(item, Item):
+            return item.money
+        else:
+            raise TypeError("Ошибка! Тип данных должен быть число или класс Item")
+
+    def __add__(self, item: Union[int,float,Optional['Item']]) -> float|int:
+        """ сложение двух классов или класса и числа """
+        return self.__check_item(item) + self.money
+
+    def __radd__(self, item: Union[int,float,Optional['Item']]) -> float|int:
+        """ сложение двух классов или класса и числа (класс справа, второе слагаемое) """
+        return self.__add__(item)
+
+
+
+class Budget:
+    def __init__(self) -> None:
+        self.list_items = list()
+
+    def add_item(self, it: Item) -> None:
+        """ добавление статьи расхода в бюджет (it - объект класса Item) """
+        self.list_items.append(it)
+
+    def remove_item(self, indx: int) -> None:
+        """ удаление статьи расхода из бюджета по его порядковому номеру indx (индексу: отсчитывается с нуля) """
+        if len(self.list_items) >= indx:
+            del self.list_items[indx]
+
+    def get_items(self) -> list:
+        """ возвращает список всех статей расходов (список из объектов класса Item) """
+        return self.list_items
+
+
+name = ""
+money = 0
+it = Item(name, money)
+
+my_budget = Budget()
+my_budget.add_item(Item("Курс по Python ООП", 2000))
+my_budget.add_item(Item("Курс по Django", 5000.01))
+my_budget.add_item(Item("Курс по NumPy", 0))
+my_budget.add_item(Item("Курс по C++", 1500.10))
+
+# вычисление общих расходов
+s = 0
+for x in my_budget.get_items():
+    s = s + x
