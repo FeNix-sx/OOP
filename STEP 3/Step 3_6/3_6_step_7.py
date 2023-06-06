45fbe9ccbfb4c26fb331cf61605ab67f9cabc7ad
FeNix <fenix_sx@mail.ru>
2023-06-06 22:48:17 +0300

    29052023 шаг 3.6.7 готово

--- STEP 3/Step 3_6/3_6_step_7.py
+++ STEP 3/Step 3_6/3_6_step_7.py
@@ -1,31 +1,103 @@
 """
-Подвиг 4. Объявите в программе класс с именем Rect (прямоугольник), объекты которого создаются командой:
+Подвиг 6. Объявите класс с именем ShopItem (товар), объекты которого создаются командой:
 
-rect = Rect(x, y, width, height)
-где x, y - координата верхнего левого угла (числа: целые или вещественные); width, height - ширина и высота прямоугольника (числа: целые или вещественные).
+item = ShopItem(name, weight, price)
+где name - название товара (строка); weight - вес товара (число: целое или вещественное); price - цена товара (число: целое или вещественное).
 
-В этом классе определите магический метод, чтобы хэши объектов класса Rect с равными width, height были равны. Например:
+Определите в этом классе магические методы:
 
-r1 = Rect(10, 5, 100, 50)
-r2 = Rect(-10, 4, 100, 50)
+__hash__() - чтобы товары с одинаковым названием (без учета регистра), весом и ценой имели бы равные хэши;
+__eq__() - чтобы объекты с одинаковыми хэшами были равны.
 
-h1, h2 = hash(r1), hash(r2)   # h1 == h2
-P.S. На экран ничего выводить не нужно, только объявить класс.
+Затем, из входного потока прочитайте строки командой:
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+Строки имеют следующий формат:
+
+название товара 1: вес_1 цена_1
+...
+название товара N: вес_N цена_N
+
+Например:
+
+Системный блок: 1500 75890.56
+Монитор Samsung: 2000 34000
+Клавиатура: 200.44 545
+Монитор Samsung: 2000 34000
+
+Как видите, товары в этом списке могут совпадать.
+
+Необходимо для всех этих строчек сформировать соответствующие объекты класса ShopItem и добавить в словарь с именем shop_items. Ключами словаря должны выступать сами объекты, а значениями - список в формате:
+
+[item, total]
+
+где item - объект класса ShopItem; total - общее количество одинаковых объектов (с одинаковыми хэшами). Подумайте, как эффективно программно наполнять такой словарь, проходя по списку lst_in один раз.
+
+P.S. На экран ничего выводить не нужно, только объявить класс и сформировать словарь.
+
+Sample Input:
+
+Системный блок: 1500 75890.56
+Монитор Samsung: 2000 34000
+Клавиатура: 200.44 545
+Монитор Samsung: 2000 34000
 """
-class Rect:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+import sys
+
+class ShopItem:
+    def __init__(self, name, weight, price):
+        self.name = name.lower()
+        self.weight = float(weight)
+        self.price = float(price)
 
     def __hash__(self):
-        return hash((self.width, self.height))
+        return hash((self.name, self.weight, self.price))
+
+    def __eq__(self, other):
+        """равно"""
+        if isinstance(other, ShopItem):
+            return self.name == other.name and self.weight == other.weight and self.price == other.price
+        raise TypeError(f"Ошибка! {type(other)}")
 
 
-r1 = Rect(10, 5, 100, 50)
-r2 = Rect(-10, 4, 100, 50)
+lst_in = ['Системный блок: 1500 75890.56',
+          'Монитор Samsung: 2000 34000',
+          'Клавиатура: 200.44 545',
+          'Монитор Samsung: 2000 34000']
 
-h1, h2 = hash(r1), hash(r2)   # h1 == h2
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+lst = [lst_in[0].split(":")[0]]
+lst.extend(lst_in[0].split(":")[1].split())
+item = lst_in[0]
+func = lambda item: [item.split(":")[0]] + item.split(":")[1].split()
 
-print(id(h1), id(h2))
+curr_lst = list(map(func, lst_in))
+
+shop_items = dict()
+for item in curr_lst:
+    key = ShopItem(*item)
+    shop_items[key] = [ShopItem(*item), list(shop_items.keys()).count(key) + 1]
+
+it1 = ShopItem('name', 10, 11)
+it2 = ShopItem('name', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+it2 = ShopItem('name', 10, 12)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('name', 11, 11)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('NAME', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+name = lst_in[0].split(':')
+for sp in shop_items.values():
+    assert isinstance(sp[0], ShopItem) and type(sp[1]) == int, "в значениях словаря shop_items первый элемент должен быть объектом класса ShopItem, а второй - целым числом"
+
+v = list(shop_items.values())
+if v[0][0].name.strip() == "Системный блок":
+    assert v[0][1] == 1 and v[1][1] == 2 and v[2][1] == 1 and len(v) == 3, "неверные значения в словаре shop_items"
+
+if v[0][0].name.strip() == "X-box":
+    assert v[0][1] == 2 and v[1][1] == 1 and v[2][1] == 2 and len(v) == 3, "неверные значения в словаре shop_items"
