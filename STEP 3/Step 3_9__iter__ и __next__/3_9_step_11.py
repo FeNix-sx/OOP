e14f4c99d9678127fff452da0c8b875765b7105b
FeNix <fenix_sx@mail.ru>
2023-10-08 23:42:11 +0300

    08/10/2023 Step 3.9.11

--- STEP 3/Step 3_9__iter__ и __next__/3_9_step_11.py
+++ STEP 3/Step 3_9__iter__ и __next__/3_9_step_11.py
@@ -5,8 +5,9 @@
 Объекты этого класса должны создаваться командой:
 
 m1 = Matrix(rows, cols, fill_value)
-где rows, cols - число строк и столбцов матрицы; fill_value - заполняемое начальное значение
-элементов матрицы (должно быть число: целое или вещественное).
+где rows, cols - число строк и столбцов матрицы;
+fill_value - заполняемое начальное значение элементов
+матрицы (должно быть число: целое или вещественное).
 Если в качестве аргументов передаются не числа, то генерировать исключение:
 raise TypeError('аргументы rows, cols - целые числа; fill_value - произвольное число')
 Также объекты можно создавать командой:
@@ -51,19 +52,22 @@ P.S. В программе нужно объявить только класс. 
 
 class Matrix:
     def __init__(self, rows: int, cols: int=None, fill_value=None)->None:
-        if fill_value is not None:
-            if not isinstance(rows, int) or not isinstance(cols, int) or not isinstance(fill_value, (int, float)):
-                raise TypeError('аргументы rows, cols - целые числа; fill_value - произвольное число')
-            self.rows = rows
-            self.cols = cols
-            self.matrix = [[fill_value] * self.cols for _ in range(self.rows)]
-        else:
-            if not isinstance(rows, list) or not all(isinstance(i, list) for i in rows) or not all(
-                    isinstance(j, (int, float)) for i in rows for j in i):
+        if isinstance(rows, list):
+            len_it = len(rows[0])
+            if not all([len_it == len(item) for item in rows]):
+                raise TypeError('список должен быть прямоугольным, состоящим из чисел')
+            if not all(all([type(item) in (int, float) for item in row]) for row in rows):
                 raise TypeError('список должен быть прямоугольным, состоящим из чисел')
             self.matrix = rows
             self.rows = len(rows)
             self.cols = len(rows[0])
+        elif type(fill_value) in (int, float) and isinstance(rows, int) and isinstance(cols, int):
+            self.rows = rows
+            self.cols = cols
+            self.matrix = [[fill_value] * self.cols for _ in range(self.rows)]
+        else:
+            raise TypeError('аргументы rows, cols - целые числа; fill_value - произвольное число')
+
 
     def __getitem__(self, indices):
         row, col = indices
@@ -84,9 +88,12 @@ class Matrix:
         self.matrix[row][col] = value
 
     def __add__(self, other):
-        if not isinstance(other, Matrix) or self.rows != other.rows or self.cols != other.cols:
+        if type(other) in (int, float):
+            result = [[self.matrix[i][j] + other for j in range(self.cols)] for i in range(self.rows)]
+        elif not isinstance(other, Matrix) or self.rows != other.rows or self.cols != other.cols:
             raise ValueError('операции возможны только с матрицами равных размеров')
-        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.cols)] for i in range(self.rows)]
+        else:
+            result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.cols)] for i in range(self.rows)]
         return Matrix(result)
 
     def __radd__(self, other):
@@ -107,10 +114,98 @@ class Matrix:
 
 
 
+list2D = [[1, 2], [3, 4], [5, 6, 7]]
+try:
+    st = Matrix(list2D)
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError для не прямоугольного списка в конструкторе Matrix"
 
+list2D = [[1, []], [3, 4], [5, 6]]
+try:
+    st = Matrix(list2D)
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError для списка не из чисел в конструкторе Matrix"
 
-mt = Matrix([[1, 2], [3, 4]])
-res = mt[0, 0] # 1
-res = mt[0, 1] # 2
-res = mt[1, 0] # 3
-res = mt[1, 1] # 4
+try:
+    st = Matrix('1', 2, 0)
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError для не числовых аргументов в конструкторе Matrix"
+
+list2D = [[1, 2], [3, 4], [5, 6]]
+matrix = Matrix(list2D)
+assert matrix[2, 1] == 6, "неверно отработал конструктор или __getitem__"
+
+matrix = Matrix(4, 5, 10)
+assert matrix[3, 4] == 10, "неверно отработал конструктор или __getitem__"
+
+try:
+    v = matrix[3, -1]
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError"
+
+try:
+    v = matrix['0', 4]
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError"
+
+matrix[0, 0] = 7
+assert matrix[0, 0] == 7, "неверно отработал __setitem__"
+
+try:
+    matrix[0, 0] = 'a'
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError в __setitem__"
+
+m1 = Matrix([[1, 2], [3, 4]])
+m2 = Matrix([[1, 1], [1, 1], [1, 1]])
+
+try:
+    matrix = m1 + m2
+except ValueError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение ValueError при сложении матриц разных размеров"
+
+m1 = Matrix([[1, 2], [3, 4]])
+m2 = Matrix([[1, 1], [1, 1]])
+matrix = m1 + m2
+assert isinstance(matrix, Matrix), "операция сложения матриц должна возвращать экземпляр класса Matrix"
+assert matrix[1, 1] == 5, "неверно отработала операция сложения матриц"
+assert m1[1, 1] == 4 and m1[0, 1] == 2 and m2[1, 1] == 1 \
+       and m2[0, 0] == 1, "исходные матрицы не должны меняться при операции сложения"
+
+m1 = Matrix(2, 2, 1)
+id_m1_old = id(m1)
+m2 = Matrix(2, 2, 1)
+m1 = m1 + m2
+id_m1_new = id(m1)
+assert id_m1_old != id_m1_new, "в результате операции сложения должен создаваться НОВЫЙ экземпляр класса Matrix"
+
+matrix = Matrix(2, 2, 0)
+m = matrix + 10
+assert matrix[0, 0] == matrix[1, 1] == 0, "исходные матрицa не должна меняться при операции сложения c числом"
+assert m[0, 0] == 10, "неверно отработала операция сложения матрицы с числом"
+
+m1 = Matrix(2, 2, 1)
+m2 = Matrix([[0, 1], [1, 0]])
+identity_matrix = m1 - m2  # должна получиться единичная матрица
+assert m1[0, 0] == 1 and m1[1, 1] == 1 and m2[0, 0] == 0 \
+       and m2[0, 1] == 1, "исходные матрицы не должны меняться при операции вычитания"
+assert identity_matrix[0, 0] == 1 and identity_matrix[1, 1] == 1, "неверно отработала операция вычитания матриц"
+
+matrix = Matrix(2, 2, 1)
+m = matrix - 1
+assert matrix[0, 0] == matrix[1, 1] == 1, "исходные матрицa не должна меняться при операции вычитания c числом"
+assert m[0, 0] == m[1, 1] == 0, "неверно отработала операция вычитания числа из матрицы"
